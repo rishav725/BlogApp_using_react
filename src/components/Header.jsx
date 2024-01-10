b08afca34d8f3803aaec6f482d979e792ae891de
Rishav <rishavshah725@gmail.com>
2024-01-10 21:16:34 +0530

    commited

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header=()=>{
+    return(
+        <div className='border shadow-md py-4  fixed relative'>
+          <header className='text-center'>
+            <h1 className='text-xl font-bold uppercase'>Codehelp Blogs</h1>
+          </header>
+        </div>
+    )
+}
+export default Header;
